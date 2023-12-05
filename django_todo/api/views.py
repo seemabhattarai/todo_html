92c95371658db4fba111cdbd2497ed460dd1b94a
Seemabhattarai <simabhattaraibca077@indrenicollege.edu.np>
2023-12-05 13:52:44 +0545

    update

--- django_todo/api/views.py
+++ django_todo/api/views.py
@@ -3,13 +3,22 @@ from todoapp.models import *
 from rest_framework.views import APIView
 from .serializers import *
 from rest_framework.response import Response
+from rest_framework import status, permissions
 
 
 class HomeView(APIView):
+    permission_classes= [permissions.IsAuthenticated]
     def get (get, request):
-        task = Task.objects.all()
-        serializer = TaskSerializer(task, many=True)
-        return Response(serializer.data)
+        userprofile = UserProfile.objects.get(user=request.user)
+        ctask = Task.objects.filter(completed=True, created_by= userprofile)
+        intask = Task.objects.filter(completed=False, created_by= userprofile)
+        cserializer = TaskSerializer(ctask, many=True).data
+        inserializer = TaskSerializer(intask, many=True).data
+        data = {
+            'completed task': cserializer,
+            'incompleted task': inserializer
+        }
+        return Response(data, status=status.HTTP_200_OK)
 
 
 # Create your views here.
